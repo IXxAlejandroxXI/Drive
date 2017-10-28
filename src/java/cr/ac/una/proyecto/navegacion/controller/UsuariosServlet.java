6aad8d90d3ce8aad591b32b5bb78c6123b38b646
aleca <aleca@ALEJANDRO-PC>
2017-10-28 15:29:19 -0600

    Secciones correctas, falta validarlo en el JSP nada mas

--- src/java/cr/ac/una/proyecto/navegacion/controller/UsuariosServlet.java
+++ src/java/cr/ac/una/proyecto/navegacion/controller/UsuariosServlet.java
@@ -43,13 +43,13 @@ public class UsuariosServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         try {
             String json;
-            
+
             //Se crea el objeto Persona
             Clientes c = new Clientes();
 
             //Se crea el objeto de la logica de negocio
             ClientesBL cBL = new ClientesBL();
-            
+
             Chofer p = new Chofer();
 
             //Se crea el objeto de la logica de negocio
@@ -65,28 +65,52 @@ public class UsuariosServlet extends HttpServlet {
                 case "validarUsuario":
                     usuario = request.getParameter("usuario");
                     password = request.getParameter("password");
-                    
+
                     c = cBL.findById(usuario);
                     p = pBL.findById(usuario);
-                    if (c.getUsuario().equals(usuario) || p.getCedula().equals(usuario)) {
-                        if ((c.getUsuario().equals(usuario)&& c.getContrasena().equals(password)) || (p.getCedula().equals(usuario) && password.equals("contraseña"))) {
-                            HttpSession session=request.getSession(true); 
-                            session.setAttribute("usuario", usuario); 
-                            session.setAttribute("loginStatus", "login"); 
-                            if (c.getUsuario().equals(usuario)) {
-                                session.setAttribute("tipo", "Administrador"); 
-                            }else{
-                                session.setAttribute("tipo", "Normal"); 
+                    if(p == null){
+                        p = new Chofer();
+                        p.setCedula("");
+                    }
+                    else{
+                        c = new Clientes();
+                        c.setUsuario("");
+                    }
+                        if (p.getCedula().equals(usuario)) {
+                        if (p.getCedula().equals(usuario) && password.equals("contraseña")) {
+                            HttpSession session = request.getSession(true);
+                            session.setAttribute("usuario", usuario);
+                            session.setAttribute("loginStatus", "login");
+                            if (p.getCedula().equals(usuario)) {
+                                session.setAttribute("tipo", "Administrador");
+                            } else {
+                                session.setAttribute("tipo", "Normal");
                             }
                             out.print("C~Validación correcta... espere esta siendo redireccionado");
-                        }else{
+                        } else {
                             out.print("E~Usuario o contraseña invalidos");
                         }
-                    }else{
-                        out.print("E~Usuario o contraseña invalidos");
+                    } else {
+                        if (c.getUsuario().equals(usuario)) {
+                            if ((c.getUsuario().equals(usuario) && c.getContrasena().equals(password))) {
+                                HttpSession session = request.getSession(true);
+                                session.setAttribute("usuario", usuario);
+                                session.setAttribute("loginStatus", "login");
+                                if (c.getUsuario().equals(usuario)) {
+                                    session.setAttribute("tipo", "Administrador");
+                                } else {
+                                    session.setAttribute("tipo", "Normal");
+                                }
+                                out.print("C~Validación correcta... espere esta siendo redireccionado");
+                            } else {
+                                out.print("E~Usuario o contraseña invalidos");
+                            }
+                        } else {
+                            out.print("E~Usuario o contraseña invalidos");
+                        }
                     }
                     break;
-                    
+
                 default:
                     out.print("E~No se indico la acción que se desea realizare");
                     break;
